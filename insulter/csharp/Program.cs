be644546642478993bda695301eb2170dc103ba8
Sam Carbine <scarbine@me.com>
2021-08-23 15:09:37 -0500

    tested out

--- insulter/csharp/Program.cs
+++ insulter/csharp/Program.cs
@@ -1,46 +1,49 @@
 using System;
 using System.Collections.Generic;
+using Linq;
 
 Main();
 
 
-void Main(){
-
-List<string> insults = new List<string>()
+void Main()
 {
-    "You look like what morning breath smells like.",
-    "If you tried to give me cpr I would probably throw myself back under water",
-    "I am not a fan of you",
-    "You think you're so special...and you are. The fact of your existence is nearly impossible to believe",
-    "I'd rather walk than be on the same plane as you",
-    "If I were given a million dollars to hang out with you...I would, but then I'd have to spend half of it on therapy",
-    "Do you have to be so...like that?"
-};
 
-int insultsLength = insults.Count();
-
-List<int> indexes = new List<int>();
-
-while (indexes.count < 3)
-{
-    int candidate = getRandomInt();
-    if (!indexes.Contains(candidate))
+    List<int> indexes = new List<int>();
+    List<string> insults = new List<string>()
     {
-        indexes.Push(candidate);
-    }
-};
+        "You look like what morning breath smells like.",
+        "If you tried to give me cpr I would probably throw myself back under water",
+        "I am not a fan of you",
+        "You think you're so special...and you are. The fact of your existence is nearly impossible to believe",
+        "I'd rather walk than be on the same plane as you",
+        "If I were given a million dollars to hang out with you...I would, but then I'd have to spend half of it on therapy",
+        "Do you have to be so...like that?"
+    };
 
-for ( int i = 0; i<indexes.count; i++)
-{
-    const index = indexes[i];
-    Console.WriteLine(insults[index]);
+    int insultsLength = insults.Count;
+
+
+    while (indexes.Count < 3)
+    {
+        int candidate = getRandomInt();
+        if (!indexes.Contains(candidate))
+        {
+            indexes.Add(candidate);
+        }
+    }
+
+    for ( int i = 0; i<indexes.Count; i++)
+    {
+        var index = indexes[i];
+        Console.WriteLine(insults[index]);
+    }
+
+    int getRandomInt(){
+        Random r = new Random();
+        int genRan = r.Next(0,insultsLength);
+        return genRan;
+    };
+
 }
 
-};
 
-
-void getRandomInt(){
-    Random r = new Random();
-    int genRan = r.Next(0, (insultsLength -1));
-    return genRan
-};
